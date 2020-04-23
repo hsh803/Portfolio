dd30a0da2758bd2616cf45990e6567783b658a99
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-04-23 18:49:36 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.chart').easyPieChart({
+    size: 110,
+    barColor: "#C8C8C8",
+    scaleLength: 0,
+    lineWidth: 8,
+    trackColor: "#ffffff",
+    lineCap: "circle",
+    animate: 2000,
+  });
+});
