538e4c3dd0bb9f9537b3671efcbdbdb3133f9f7d
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-04-25 14:11:45 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,25 @@
 $(function() {
-  $('.chart').easyPieChart({
-    size: 110,
-    barColor: "#C8C8C8",
-    scaleLength: 0,
-    lineWidth: 8,
-    trackColor: "#ffffff",
-    lineCap: "circle",
-    animate: 2000,
-  });
+  if($(window).width() <= 420) {
+    $('.chart').easyPieChart({
+      size: 75,
+      barColor: "#C8C8C8",
+      scaleLength: 0,
+      lineWidth: 5,
+      trackColor: "#ffffff",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  } 
+  else {
+    $('.chart').easyPieChart({
+      size: 100,
+      barColor: "#C8C8C8",
+      scaleLength: 0,
+      lineWidth: 8,
+      trackColor: "#ffffff",
+      lineCap: "circle",
+      animate: 2000,
+    }); 
+  }
+
 });
